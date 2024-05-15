6282b7e7d2ef602c39879350f0d9eb21e22ca67a
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-05-15 09:06:48 -0400

    white space cleanup

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const api = require ('./public/assets/js');
+const routes = require ('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
